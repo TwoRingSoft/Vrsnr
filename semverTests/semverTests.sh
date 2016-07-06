add50b892ebc049a4fd793aa694e811ba3ccfe3f
Andrew McKnight <amcknight2718@gmail.com>
2016-07-05 23:50:55 -0400

    add test cases and baseline results for --try

--- semverTests/semverTests.sh
+++ semverTests/semverTests.sh
@@ -15,6 +15,7 @@ function runTest() {
     SMVR_TEST_FILE="${2}"
     SMVR_TEST_KEY="${3}"
     SMVR_TEST_SEMVER_COMPONENT="${4}"
+    SMVR_TEST_OPTIONS="${5}"
 
     SMVR_BACKUP_TEST_FILE="${SMVR_TEST_FILE}.orig"
 
@@ -30,7 +31,7 @@ function runTest() {
     cp "${SMVR_TEST_FILE}" "${SMVR_BACKUP_TEST_FILE}"
 
     # perform the SMVR
-    eval "${SMVR_LOCATION}/semver ${SMVR_TEST_SEMVER_COMPONENT} --file ${SMVR_TEST_FILE} --key ${SMVR_TEST_KEY}"
+    eval "${SMVR_LOCATION}/semver ${SMVR_TEST_SEMVER_COMPONENT} --file ${SMVR_TEST_FILE} --key ${SMVR_TEST_KEY} ${SMVR_TEST_OPTIONS}"
 
     # compare the modified file to copy of original
     diff "${SMVR_TEST_FILE}" "${SMVR_BACKUP_TEST_FILE}" > "${SMVR_TEST_RESULTS_FILE}"
@@ -49,12 +50,24 @@ function runTest() {
 
 }
 
-runTest "plist-semver-major" "semverTests/Fixtures/Sample.plist" "CFBundleShortVersionString" "major"
-runTest "plist-semver-minor" "semverTests/Fixtures/Sample.plist" "CFBundleShortVersionString" "minor"
-runTest "plist-semver-patch" "semverTests/Fixtures/Sample.plist" "CFBundleShortVersionString" "patch"
-runTest "plist-numerical"    "semverTests/Fixtures/Sample.plist" "CFBundleVersion"            "--numeric"
+# plist tests
+runTest "plist-semver-major" "semverTests/Fixtures/Sample.plist" "CFBundleShortVersionString" "major"       ""
+runTest "plist-semver-minor" "semverTests/Fixtures/Sample.plist" "CFBundleShortVersionString" "minor"       ""
+runTest "plist-semver-patch" "semverTests/Fixtures/Sample.plist" "CFBundleShortVersionString" "patch"       ""
+runTest "plist-numerical"    "semverTests/Fixtures/Sample.plist" "CFBundleVersion"            "--numeric"   ""
 
-runTest "xcconfig-semver-major" "semverTests/Fixtures/Sample.xcconfig" "CURRENT_PROJECT_VERSION" "major"
-runTest "xcconfig-semver-minor" "semverTests/Fixtures/Sample.xcconfig" "CURRENT_PROJECT_VERSION" "minor"
-runTest "xcconfig-semver-patch" "semverTests/Fixtures/Sample.xcconfig" "CURRENT_PROJECT_VERSION" "patch"
-runTest "xcconfig-numerical"    "semverTests/Fixtures/Sample.xcconfig" "DYLIB_CURRENT_VERSION"   "--numeric"
+runTest "plist-semver-major-try" "semverTests/Fixtures/Sample.plist" "CFBundleShortVersionString" "major"       "--try"
+runTest "plist-semver-minor-try" "semverTests/Fixtures/Sample.plist" "CFBundleShortVersionString" "minor"       "--try"
+runTest "plist-semver-patch-try" "semverTests/Fixtures/Sample.plist" "CFBundleShortVersionString" "patch"       "--try"
+runTest "plist-numerical-try"    "semverTests/Fixtures/Sample.plist" "CFBundleVersion"            "--numeric"   "--try"
+
+# xcconfig tests
+runTest "xcconfig-semver-major" "semverTests/Fixtures/Sample.xcconfig" "CURRENT_PROJECT_VERSION" "major"        ""
+runTest "xcconfig-semver-minor" "semverTests/Fixtures/Sample.xcconfig" "CURRENT_PROJECT_VERSION" "minor"        ""
+runTest "xcconfig-semver-patch" "semverTests/Fixtures/Sample.xcconfig" "CURRENT_PROJECT_VERSION" "patch"        ""
+runTest "xcconfig-numerical"    "semverTests/Fixtures/Sample.xcconfig" "DYLIB_CURRENT_VERSION"   "--numeric"    ""
+
+runTest "xcconfig-semver-major-try" "semverTests/Fixtures/Sample.xcconfig" "CURRENT_PROJECT_VERSION" "major"        "--try"
+runTest "xcconfig-semver-minor-try" "semverTests/Fixtures/Sample.xcconfig" "CURRENT_PROJECT_VERSION" "minor"        "--try"
+runTest "xcconfig-semver-patch-try" "semverTests/Fixtures/Sample.xcconfig" "CURRENT_PROJECT_VERSION" "patch"        "--try"
+runTest "xcconfig-numerical-try"    "semverTests/Fixtures/Sample.xcconfig" "DYLIB_CURRENT_VERSION"   "--numeric"    "--try"
